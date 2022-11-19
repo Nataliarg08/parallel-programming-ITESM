6d0ab686f37b042af9520bbac34b5a6a40e5c47e
Natalia Rodríguez González <80491340+Nataliarg08@users.noreply.github.com>
2022-11-19 10:16:41 -0600

    Create plot.m

---
+++ practice_3/plot.m
@@ -0,0 +1,23 @@
+clearvars
+close all
+clc
+
+c = 4;
+L = 2;
+deltat = 0.125000;
+k = 0:99;
+t = k*deltat;
+x = linspace(0,6,50);
+xp = c*pi/L^2;
+[T,X] = meshgrid(t,x);
+u = zeros(size(T));
+for n = 1:1000
+    u = u + ((-1)^(n+1)/n) * (sin(n*pi*X/L).*exp(-n*xp*T));
+end
+figure(1)
+surf(T, X, u)
+grid on
+xlabel('\itt\rm')
+ylabel('\itx\rm')
+zlabel('\itu(x,t)\rm')
+view(125, 35)
